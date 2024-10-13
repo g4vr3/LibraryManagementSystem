bd3715457211ce6050ae246b894dc1dfafd07119
g4vr3 <gdfizan@gmail.com>
2024-10-13 16:32:27 +0200

    Updated JavaDoc

--- JavaDoc/package-search-index.js
+++ JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"autor"},{"l":"libro"},{"l":"main"},{"l":"prestamo"},{"l":"usuario"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"autor"},{"l":"jdbc"},{"l":"libro"},{"l":"main"},{"l":"prestamo"},{"l":"usuario"}];updateSearchResults();
